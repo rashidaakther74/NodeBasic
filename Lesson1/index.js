e1d7f67f2116f7df6e7cb9a5fe9fd0f27e968f0f
rashidarashida <rashidaakther774@gmail.com>
2025-10-13 15:22:16 -0700

    Lesson1

---
+++ Lesson1/index.js
@@ -0,0 +1,5 @@
+const s1 = require('./student')
+
+console.log(s1.getname())
+console.log(s1.getAge())
+console.log(s1.result)
---
+++ Lesson1/student.js
@@ -0,0 +1,13 @@
+const getName = () =>{
+    return "Rashida Akther";
+}
+
+const getAge = () => {
+    return "30"
+}
+
+const cgpa = 3.65;
+
+exports.getname = getName;
+exports.getAge = getAge;
+exports.result = cgpa;
